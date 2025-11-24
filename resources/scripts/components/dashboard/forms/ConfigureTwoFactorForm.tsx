2151693a0eb120fe0c9c88201f2dc702e5b3ac3a
tarboox45-cpu <tarboox45@gmail.com>
2025-11-24 21:13:07 +0200

    Add files via upload

--- resources/scripts/components/dashboard/forms/ConfigureTwoFactorForm.tsx
+++ resources/scripts/components/dashboard/forms/ConfigureTwoFactorForm.tsx
@@ -1,1 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { useStoreState } from 'easy-peasy';
+import { ApplicationStore } from '@/state';
+import tw from 'twin.macro';
+import { Button } from '@/components/elements/button/index';
+import SetupTOTPDialog from '@/components/dashboard/forms/SetupTOTPDialog';
+import RecoveryTokensDialog from '@/components/dashboard/forms/RecoveryTokensDialog';
+import DisableTOTPDialog from '@/components/dashboard/forms/DisableTOTPDialog';
+import { useFlashKey } from '@/plugins/useFlash';
 
+export default () => {
+    const [tokens, setTokens] = useState<string[]>([]);
+    const [visible, setVisible] = useState<'enable' | 'disable' | null>(null);
+    const isEnabled = useStoreState((state: ApplicationStore) => state.user.data!.useTotp);
+    const { clearAndAddHttpError } = useFlashKey('account:two-step');
+
+    useEffect(() => {
+        return () => {
+            clearAndAddHttpError();
+        };
+    }, [visible]);
+
+    const onTokens = (tokens: string[]) => {
+        setTokens(tokens);
+        setVisible(null);
+    };
+
+    return (
+        <div className="w-full flex justify-between items-center mt-2">
+            <SetupTOTPDialog open={visible === 'enable'} onClose={() => setVisible(null)} onTokens={onTokens} />
+            <RecoveryTokensDialog tokens={tokens} open={tokens.length > 0} onClose={() => setTokens([])} />
+            <DisableTOTPDialog open={visible === 'disable'} onClose={() => setVisible(null)} />
+            <div>
+                <p className="text-sm opacity-50">Two-step verification</p>
+                <p>
+                {isEnabled
+                    ? 'Verification is currently enabled on your account'
+                    : 'You don\'t currently have two-step verification'}
+                </p>
+            </div>
+            {isEnabled ? (
+                <Button.Danger onClick={() => setVisible('disable')}>Disable</Button.Danger>
+            ) : (
+                <Button onClick={() => setVisible('enable')}>Enable</Button>
+            )}
+        </div>
+    );
+};
